c2579278ffadbd9676d94bbb654bd1752e661e88
Default Programmer <defproger@gmail.com>
2024-07-07 16:41:36 +0300

    author screen

---
+++ assets/js/slideIn.js
@@ -0,0 +1,17 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('show');
+        }
+    });
+}
+
+let options = {
+    threshold: [0.1]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.slideIn');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
