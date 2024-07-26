621a0cbad95d5016e2917075e2c2167775dfd81f
Default Programmer <defproger@gmail.com>
2024-07-26 14:50:23 +0300

    slideIn animations

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,1 +1,13 @@
-    $('#quotes_slinky').parallax();
+$('#quotes_slinky').parallax();
+
+$(window).on('scroll', function () {
+    let wHeight = $(window).height() - 100;
+    let scrollTop = $(window).scrollTop() - 100;
+    $('.pattern_line').css('transform', 'translate3d(0px, -' + (scrollTop - wHeight) * 0.75 + 'px , 0px)');
+});
+
+$(window).on('scroll', function () {
+    let wHeight = $(window).height() - 100;
+    let scrollTop = $(window).scrollTop() - 100;
+    $('.after_quote').css('transform', 'translate3d(0px, -' + (scrollTop - wHeight) * 0.75 + 'px , 0px)');
+});
