9fdce3a950a7ce4e2db18d28b320d462a95022e7
Default Programmer <defproger@gmail.com>
2024-08-24 20:09:20 +0300

    payment table

--- app/base.sql
+++ app/base.sql
@@ -11,6 +11,20 @@ create table gallery
     price       int          null
 );
 
+create table payments
+(
+    id      int auto_increment
+        primary key,
+    art_id  int          not null,
+    email   varchar(255) not null,
+    name    varchar(255) not null,
+    phone   varchar(30)  not null,
+    address varchar(255) not null,
+    message text         null,
+    method  varchar(20)  not null comment 'paypal
+credit_card'
+);
+
 INSERT INTO gallery (id, name, image, type, size, description, status, price) VALUES (1, 'The destruction Painting', '1.jpg', 'OIL ON CANVAS', '130 W x 160 H x 1 D cm', 'Sit amet, consectetur adipiscing elit. Praesent at cursus magna, at dapibus eros. Aliquam tincidunt tortor eu placerat vehicula. Curabitur feugiat faucibus mauris id ullamcorper.', 'available', 5000);
 INSERT INTO gallery (id, name, image, type, size, description, status, price) VALUES (2, 'The destruction Painting', '2.jpg', 'OIL ON CANVAS', '130 W x 160 H x 1 D cm', 'Sit amet, consectetur adipiscing elit. Praesent at cursus magna, at dapibus eros. Aliquam tincidunt tortor eu placerat vehicula. Curabitur feugiat faucibus mauris id ullamcorper.', 'in private collection', 5000);
 INSERT INTO gallery (id, name, image, type, size, description, status, price) VALUES (3, 'The destruction Painting', '3.jpg', 'OIL ON CANVAS', '130 W x 160 H x 1 D cm', 'Sit amet, consectetur adipiscing elit. Praesent at cursus magna, at dapibus eros. Aliquam tincidunt tortor eu placerat vehicula. Curabitur feugiat faucibus mauris id ullamcorper.', 'sold', 5000);
--- app/init.php
+++ app/init.php
@@ -4,7 +4,7 @@ const SETTINGS = [
     'db_user' => 'user',
     'db_password' => 'pass',
     'db_name' => 'base',
-    'db_host' => 'localhost',
+    'db_host' => 'db',
     'db_port' => '3306',
 
     'client_id' => 'AQehiNbkCf5PozKkcpVzTIHDpCS3Kbew8Q-eyM74YWtllX8S3jCF-M-z5hKuqhEVB7lBye_tpgfmJZMR',
