346f0590d566d79b62e0e783c07a5b21780ae650
Default Programmer <defproger@gmail.com>
2024-08-28 19:07:57 +0300

    payment deny

--- cancelOrder.php
+++ cancelOrder.php
@@ -1,3 +1,20 @@
+<?php
+require_once 'app/db.php';
+if (!empty($_GET['id']) && !empty($_GET['token'])) {
+    list($id, $hash) = explode('_', $_GET['id'], 2);
+    $payment = query("select * from payments where id = :id and hash = :hash and pay_id = :token", [
+        'id' => $id,
+        'hash' => $hash,
+        'token' => $_GET['token']
+    ]);
+
+    if (!empty($payment['id']) && $payment['status'] == 'created') {
+        db_update('payments', $payment['id'], [
+            'status' => 'canceled'
+        ]);
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,13 +24,13 @@
     <link rel="stylesheet" href="assets/css/messages.css">
 </head>
 <body>
-    <div class="container">
-        <h1>PAYMENT ERROR</h1>
-        <br>
-        <div class="error-box">
+<div class="container">
+    <h1>PAYMENT ERROR</h1>
+    <br>
+    <div class="error-box">
 
-            <p>A transaction error occurred and the transaction was not executed. Repeat the payment attempt.</p>
-        </div>
+        <p>A transaction error occurred and the transaction was not executed. Repeat the payment attempt.</p>
     </div>
+</div>
 </body>
 </html>
