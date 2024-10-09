bafc5bbf8544b819b840db332e3ba4416446a007
Default Programmer <defproger@gmail.com>
2024-10-09 17:18:16 +0300

    fix payment data

--- app/init.php
+++ app/init.php
@@ -8,10 +8,6 @@ const SETTINGS = [
     'db_port' => '3306',
 
     'client_id' => 'AQehiNbkCf5PozKkcpVzTIHDpCS3Kbew8Q-eyM74YWtllX8S3jCF-M-z5hKuqhEVB7lBye_tpgfmJZMR',
-    'client_secret' => 'EFJk4nBBr8J-0MnbkdztA32BA9YoKYx9JDHWbuPu_C0lROYZ3EGqexDzokly1vawWNlfVpl2f9MqBRGB',
-
-    'pay_mode' => 'sandbox', // 'sandbox|live'
-    'log_level' => 'DEBUG', // 'DEBUG|INFO'
 
     'domain' => 'localhost:8080',
 ];
